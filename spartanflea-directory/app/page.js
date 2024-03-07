33b197563a1a949d222af965c7c47a603107b4c2
PioRomo <promocolleges@gmail.com>
2024-03-07 10:27:35 -0800

    trying to add images

--- spartanflea-directory/app/page.js
+++ spartanflea-directory/app/page.js
@@ -2,10 +2,12 @@
 import {Alfa_Slab_One} from 'next/font/google'; 
 
 import MainLayout from './layouts/MainLayout.js'
+import CarouselComp from './components/CarouselComp.js';
+
 export default function Home() {
   return (
     <MainLayout>
-
+      <CarouselComp /> 
     </MainLayout>
   );
 }
