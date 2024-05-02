6d743b3430a7c25afa905232e58706f88fbe47e4
PioRomo <promocolleges@gmail.com>
2024-05-01 19:09:01 -0700

    Tried to implement functionality

--- spartanflea-directory/app/layouts/includes/MainHeader.js
+++ spartanflea-directory/app/layouts/includes/MainHeader.js
@@ -3,7 +3,48 @@ import Link from "next/link";
 import {AiOutlineSearch} from "react-icons/ai";
 import {BiLoaderCircle} from "react-icons/bi";
 
+import { useState } from "react";
+
 export default function MainHeader(){
+
+
+    const [items, setItems] = useState([])
+    const [isSearching, setIsSearching] = useState(null)
+
+    const debounce = (func, delay) => {
+        let timeoutId;
+        return function (...args) {
+            if (timeoutId) clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                func.apply(this, args);
+            }, delay);
+        };
+    };
+
+    const handleSearchName = debounce(async (event) => {
+        if (event.target.value == "") {
+            setItems([])
+            return
+        }
+
+        setIsSearching(true)
+
+        try {
+            const response = await fetch(`/api/products/search-by-name/${event.target.value}`)
+            const result = await response.json()
+
+            if (result) {
+                setItems(result)
+                setIsSearching(false)
+                return
+            }
+            setItems([])
+            setIsSearching(false)
+        } catch (error) {
+            console.log(error)
+            alert(error)
+        }
+    }, 500)
 
     return(
         <>
@@ -20,10 +61,36 @@ export default function MainHeader(){
                                     <div className="flex items-center">
                                         {/*Search Bar */}
                                         <div className="rounded-full relative flex items-center border-2 border-gray-900 w-full p-2 mt-1 ">
-                        
                                             <input
-                                             className="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none" placeholder="Search" type="text"
+                                                className="w-full 
+                                                placeholder-gray-400 
+                                                text-sm pl-3 
+                                                focus:outline-none" 
+                                                onChange={handleSearchName}
+                                                placeholder="Search" 
+                                                type="text"
                                             />
+
+{isSearching ? <BiLoaderCircle className="mr-2 animate-spin" size={22} /> : null}
+                                        
+                                        {items.length > 0 ?
+                                            <div className="absolute bg-white max-w-[910px] h-auto w-full z-20 left-0 top-12 border p-1">
+                                                {items.map((item) => (
+                                                    <div className="p-1" key={item.id}>
+                                                        <Link 
+                                                            href={`/product/${item?.id}`}
+                                                            className="flex items-center justify-between w-full cursor-pointer hover:bg-gray-200 p-1 px-2"
+                                                        >
+                                                            <div className="flex items-center">
+                                                                <img className="rounded-md" width="40" src={item?.url+'/40'} />
+                                                                <div className="truncate ml-2">{ item?.title }</div>
+                                                            </div>
+                                                            <div className="truncate">£{ (item?.price / 100).toFixed(2) }</div>
+                                                        </Link>
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        : null}
 
                                         </div>
                                         <button className=" rounded-full mt-1 flex items-center bg-blue-600 text-sm font-semibold text-white p-[11px] ml-2">
@@ -32,9 +99,6 @@ export default function MainHeader(){
                                             </button>
                                         </button>
 
-                                        <div className="ml-2 mt-1 text-xs px-2 hover:text-blue-500 cursor-pointer">
-                                            Advanced
-                                        </div>
                                     </div>
                                 </div>
                             </div>
